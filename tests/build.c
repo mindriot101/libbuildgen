01da09c07f1948107b90709fefbabb9b0207ddd5
Simon Walker <s.r.walker101@googlemail.com>
2017-11-02 18:13:52 +0000

    Initial commit

---
+++ tests/build.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <buildgen.h>
+
+int main(int argc, char *argv[]) {
+    char *source_file = "../tests/build.c";
+    char *output_binary = "./foo";
+
+
+    return 0;
+}
